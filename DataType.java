276e6f6eef5bba9ea43547459be517a0dc27c5a2
Sakthikumar044 <139200795+Sakthikumar044@users.noreply.github.com>
2025-06-25 17:05:58 +0530

    Day2

---
+++ DataType.java
@@ -0,0 +1,27 @@
+package day2;
+
+public class DataType {
+	public static void main(String args[]) {
+		
+		byte age = 21;
+		System.out.println("byte is : " +age);
+		short num = 30000;
+		System.out.println("short is : " +num);
+		int salary = 50000;
+		System.out.println("int is : "+salary);
+		long phoneno = 1234567890l;
+		System.out.println("long is : "+phoneno);
+		
+		boolean istrue = true;
+		System.out.println("boolean is : " +istrue);
+		char A = 'B';
+		System.out.println("char is : " +A);
+		float C = 12345.123f;
+		System.out.println("float is : "+C);
+		double E = 12345.1234567;
+		System.out.println("double is : "+E);
+		
+		
+	}
+
+}
