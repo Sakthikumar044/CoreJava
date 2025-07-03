bcc2130f8873da4ec9654b1f2259c476fa2d0c62
Sakthikumar044 <139200795+Sakthikumar044@users.noreply.github.com>
2025-07-03 16:14:36 +0530

    LoopEx.java

--- LoopEx.java
+++ LoopEx.java
@@ -3,19 +3,19 @@ package day7;
 public class LoopEx {
 	public static void main(String[] args) {
 		
-//		for(int i=1; i<=1000; i++) {
-//			System.out.println("Iteration on Increament :"+i);
-//			
-//		}
-//		
-//		for(int j=1000; j>=1; j--) {
-//			System.out.println("Iteration on Decreament :"+j);
-//			
-//		}
-//		for(int i=50; i>=1; i--) {
-//			System.out.println("Reverse order :"+i);
-//			
-//		}
+		for(int i=1; i<=1000; i++) {
+			System.out.println("Iteration on Increament :"+i);
+			
+		}
+		
+		for(int j=1000; j>=1; j--) {
+			System.out.println("Iteration on Decreament :"+j);
+			
+		}
+		for(int i=50; i>=1; i--) {
+			System.out.println("Reverse order :"+i);
+			
+		}
 		int count=0;
 		for(int a=1; a<=100; a+=2) {
 			count = count+a;
